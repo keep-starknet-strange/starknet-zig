a26d5f7e7af5c853cc7a36a67a10e2646d23a51a
Thomas Coratger <thomas.coratger@gmail.com>
2024-03-12 00:41:51 +0100

    Add one more unit test for pedersen hash

--- src/crypto/pedersen_hash.zig
+++ src/crypto/pedersen_hash.zig
@@ -14,11 +14,8 @@ const SHIFT_POINT = ProjectivePoint.fromAffinePoint(&CurveParams.SHIFT_POINT);
 fn boolsToUsizeLe(bits: []const bool) usize {
     var result: usize = 0;
     for (bits, 0..) |bit, i| {
-        if (bit) {
-            result += @as(usize, 1) << @intCast(i);
-        }
+        if (bit) result += @as(usize, 1) << @intCast(i);
     }
-
     return result;
 }
 
@@ -56,19 +53,28 @@ pub fn pedersenHash(x: Felt252, y: Felt252) Felt252 {
 
 test "Pedersen hash" {
     //   Test case ported from:
-    //   https://github.com/starkware-libs/crypto-cpp/blob/95864fbe11d5287e345432dbe1e80dea3c35fc58/src/starkware/crypto/ffi/crypto_lib_test.go
-    const in1 = Felt252.fromInt(
-        u256,
-        0x03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb,
-    );
-    const in2 = Felt252.fromInt(
-        u256,
-        0x0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a,
-    );
-    const expected_hash = Felt252.fromInt(
-        u256,
-        0x030e480bed5fe53fa909cc0f8c4d99b8f9f2c016be4c41e13a4848797979c662,
-    );
+    //   https://github.com/starkware-libs/starkex-for-spot-trading/blob/master/src/starkware/crypto/starkware/crypto/signature/test/config/signature_test_data.json
 
-    try std.testing.expectEqual(expected_hash, pedersenHash(in1, in2));
+    const in = [_]std.meta.Tuple(&.{ u256, u256 }){
+        .{
+            0x03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb,
+            0x0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a,
+        },
+        .{
+            0x58f580910a6ca59b28927c08fe6c43e2e303ca384badc365795fc645d479d45,
+            0x78734f65a067be9bdb39de18434d71e79f7b6466a4b66bbd979ab9e7515fe0b,
+        },
+    };
+
+    const out = [_]u256{
+        0x30e480bed5fe53fa909cc0f8c4d99b8f9f2c016be4c41e13a4848797979c662,
+        0x68cc0b76cddd1dd4ed2301ada9b7c872b23875d5ff837b3a87993e0d9996b87,
+    };
+
+    for (in, out) |i, o| {
+        try std.testing.expectEqual(
+            Felt252.fromInt(u256, o),
+            pedersenHash(Felt252.fromInt(u256, i[0]), Felt252.fromInt(u256, i[1])),
+        );
+    }
 }
